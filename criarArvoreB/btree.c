4e60813d446cdb14a1363acececc3e3f486413cf
Kepler Batista Alves <83622985+kepler4567@users.noreply.github.com>
2021-12-02 21:03:33 -0300

    update tree.c

--- criarArvoreB/btree.c
+++ criarArvoreB/btree.c
@@ -198,13 +198,14 @@ int insere(int rrn_Pagina_Atual, char chave, int *pagina_filha_da_direita, int *
 
 //--------------Funções Principais BTREE--------------------//
 
-int gerenciador(char Arquivo[])
+iint gerenciador(char *Arquivo)
 {
-
     FILE *entrada;
     PAG novaPagina;
     FILE *Btree;
-    int rrn_Pagina_Atual, chave, aux, filho_d_pro, chave_promovida, rrn;
+    int rrn_Pagina_Atual, aux, filho_d_pro, chave_promovida, rrn;
+    int chave;
+    
 
     if ((entrada = fopen(Arquivo, "r")) == NULL)
     {
@@ -215,12 +216,12 @@ int gerenciador(char Arquivo[])
 
     if ((Btree = fopen("Btree.dat", "r+b")))
     {
-
+        
         rrn_Pagina_Atual = fgetc(Btree);
     }
     else
     {
-
+        
         Btree = fopen("Btree.dat", "w+b");
         rrn_Pagina_Atual = 0;
         fwrite(&rrn_Pagina_Atual, sizeof(int), 1, Btree);
@@ -228,28 +229,31 @@ int gerenciador(char Arquivo[])
         escreve_pagina(rrn_Pagina_Atual, novaPagina);
     }
 
-    fseek(Btree, 0, SEEK_SET);
-    fread(&chave, sizeof(int), 1, Btree);
+    fscanf(entrada, "%i", &chave);
+    printf("%d", chave);
+    int chaveTeste;
 
-    while (fscanf(Btree, "%d|", &aux) != EOF)
-    {
-        if (insere(rrn_Pagina_Atual, chave, &filho_d_pro, &chave_promovida) == ComPromocao)
-        {
-            Inicializa_pagina(&novaPagina);
-            novaPagina.chave[0] = chave_promovida;
-            novaPagina.filhos[0] = rrn_Pagina_Atual;
-            novaPagina.filhos[1] = filho_d_pro;
-            escreve_pagina(rrn_Pagina_Atual, novaPagina);
-            rrn = RRN_novapag();
-            rrn_Pagina_Atual = rrn;
-        }
-        // fseek(Btree,sizeof(int),SEEK_CUR);
-        fread(&chave, sizeof(int), 1, Btree);
-    }
-    fwrite(&rrn_Pagina_Atual, sizeof(int), 1, Btree);
-    fclose(Btree);
+    // while (fscanf(entrada, "%i|", &chaveTeste) != EOF)
+    // {
+
+    //     printf("%i", chaveTeste);
+    //     if (insere(rrn_Pagina_Atual, chave, &filho_d_pro, &chave_promovida) == ComPromocao)
+    //     {
+    //         Inicializa_pagina(&novaPagina);
+    //         novaPagina.chave[0] = chave_promovida;
+    //         novaPagina.filhos[0] = rrn_Pagina_Atual;
+    //         novaPagina.filhos[1] = filho_d_pro;
+    //         escreve_pagina(rrn_Pagina_Atual, novaPagina);
+    //         rrn = RRN_novapag();
+    //         rrn_Pagina_Atual = rrn;
+    //     }
+        fseek(entrada,sizeof(int)+1,SEEK_CUR);
+        fscanf(entrada, "%i", &chave);
+        printf("%d", chave);
+    // }
+    // fwrite(&rrn_Pagina_Atual, sizeof(int), 1, Btree);
+    // fclose(Btree);
 }
-
 void impressaoArvoreB(char *arquivo)
 {
 }
